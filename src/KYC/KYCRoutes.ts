35e1ce0bb42d56ccec3f444ef34513a9cec7066a
notdennis <58556466+DennisJunior247@users.noreply.github.com>
2024-03-17 03:28:59 +0000

    removed unused code

--- src/KYC/KYCRoutes.ts
+++ src/KYC/KYCRoutes.ts
@@ -1,18 +1,8 @@
 import express from 'express';
 import KYCService from './KYCService';
-// import VirtualCardController from './VirtualAccountController';
-// import { ValidateRequest } from '../Resources/middleware/ValidateRequest';
-// import {
-//     // VirtualAccount,
-// } from './VirtualAccountSchema';
 
 const KYCRouter = express.Router();
 
-KYCRouter
-    .post('/kyc',
-        // ValidateRequest(VirtualAccount),
-        KYCService.verifyBvnNumber)
-
-
+KYCRouter.post('/kyc', KYCService.verifyBvnNumber)
 
 export default KYCRouter;
